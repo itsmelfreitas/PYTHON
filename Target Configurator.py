f1531e4fac685ceb5e073f0e9b0373b3437b4115
Mel <itsmelfreitas@gmail.com>
2026-08-11 20:42:31 -0300

    Target Configurator
    Mock script that sets up target IP addresses and port numbers (Focus: Variables & Data Types)

---
+++ Target Configurator.py
@@ -0,0 +1,5 @@
+target_ip = "192.168.1.100"
+target_port = 8080
+print(f"Target acquired at IP Adress {target_ip} and Port Number {target_port}")
+if target_port == 8080:
+    print("Alternate web port targeted!")
