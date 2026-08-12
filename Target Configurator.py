bd0c31a814cdaa504a0213321bafe598d1948d6d
Mel <itsmelfreitas@gmail.com>
2026-08-11 21:26:54 -0300

    Update Target Configurator.py
    Updated target IP and port to be user inputs and added a message for standard port targeting.

--- Target Configurator.py
+++ Target Configurator.py
@@ -1,5 +1,8 @@
-target_ip = "192.168.1.100"
-target_port = 8080
-print(f"Target acquired at IP Adress {target_ip} and Port Number {target_port}")
+target_ip = input("What is the target IP? ")
+target_port = int(input("What is the target Port? "))
+
+print(f"Target acquired at IP Address {target_ip} and Port Number {target_port}.")
 if target_port == 8080:
     print("Alternate web port targeted!")
+else:
+    print("Standard port targeted.")
