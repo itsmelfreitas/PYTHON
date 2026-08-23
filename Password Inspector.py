d5852ad5aa2fcd62d6ba33d98e9403515b6057d2
Mel <itsmelfreitas@gmail.com>
2026-08-23 13:45:29 -0300

    Delete Password Inspector.py

--- Password Inspector.py
+++
@@ -1,7 +0,0 @@
-user_password = input("Enter your password: ")
-password_length = len(user_password)
-
-if password_length < 8:
-    print("Password is too short. Please enter at least eight characters.")
-else:
-    print("Password length is acceptable.")
